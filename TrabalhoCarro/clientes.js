e0beab89f3b9b75a2a97465a343c66cfcd1ef027
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-08-23 17:19:09 -0300

    Delete TrabalhoCarro directory

--- TrabalhoCarro/clientes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pessoa_1 = require("./pessoa");
-var amigo = new pessoa_1.Pessoa("Marta", "Silveira", "fabiano", "Katiana", 948394839, 8343848, 2004, 121201303);
-amigo.GetPessoa();
-amigo.SetPessoa();
-amigo.GetPessoa();
-amigo.VerificaIdade();
--- TrabalhoCarro/clientes.ts
+++
@@ -1,7 +0,0 @@
-import { Pessoa } from "./pessoa";
-
-let amigo = new Pessoa("Marta", "Silveira", "fabiano", "Katiana", 948394839, 8343848, 2004,121201303)
-amigo.GetPessoa()
-amigo.SetPessoa()
-amigo.GetPessoa()
-amigo.VerificaIdade()
